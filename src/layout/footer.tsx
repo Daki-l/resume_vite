018d0ebb4bb472dc7ff72a1b7adbef744759c336
liuqi <13761299894@163.com>
2026-03-19 11:35:00 +0800

    仓库位置调整

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -3,22 +3,22 @@ import { GithubFilled } from '@ant-design/icons';
 import './footer.less';
 
 const Footer: React.FC = () => (
-  <footer>
-    <div>
-      <span>Made with ❤️</span>
-      <span className="author" style={{ marginLeft: 8 }}>
-        by <a href="https://github.com/visiky/resume" target="_blank" rel="noreferrer">visiky</a>
-      </span>
-    </div>
-    <a
-      href="https://github.com/visiky/resume.git"
-      style={{ position: 'absolute', right: '8px', fontSize: '12px' }}
-      target="_blank"
-      rel="noreferrer"
-    >
-      <GithubFilled style={{ color: '#fff', marginRight: '4px' }} /> 项目代码
-    </a>
-  </footer>
+	<footer>
+		<div>
+			<span>Made with ❤️</span>
+			<span className="author" style={{ marginLeft: 8 }}>
+				by <a href="https://github.com/daki-l/resume_vite" target="_blank" rel="noreferrer">daki-l</a>
+			</span>
+		</div>
+		<a
+			href="https://github.com/daki-l/resume_vite.git"
+			style={{ position: 'absolute', right: '8px', fontSize: '12px' }}
+			target="_blank"
+			rel="noreferrer"
+		>
+			<GithubFilled style={{ color: '#fff', marginRight: '4px' }} /> 项目代码
+		</a>
+	</footer>
 );
 
 export default Footer;
